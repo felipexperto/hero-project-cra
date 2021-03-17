c0e7dddbe521557ab070b7fd77e402cbe208bd6f
Felipe Marciano <felipexperto@gmail.com>
2021-03-17 16:05:29 -0300

    fix: Adicionando custom render para lidar com styled-components em testes com Jest
    + Adicionando dependnência jest-styled-components;
    + Excluindo arquivos src/components/**/styles.js do coverage;

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,24 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
+// Custom Render for StyledComponents
+// https://testing-library.com/docs/react-testing-library/setup#custom-render
+
 import '@testing-library/jest-dom';
+import 'jest-styled-components';
+import { render } from '@testing-library/react';
+
+import { Root } from 'components/Root';
+
+const WrapperProvider = ({ children }) => (
+  <Root>{children}</Root>
+);
+
+const customRender = (ui, options) => render(ui, { wrapper: WrapperProvider, ...options });
+
+export * from '@testing-library/react';
+
+export {
+  customRender as render,
+};
