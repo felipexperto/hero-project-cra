139a698cf41ef0f7f1cee5e6bd8df336e4cfc2f8
Felipe Marciano <felipexperto@gmail.com>
2021-03-22 00:14:09 -0300

    feat: Página simples de Erro 404

--- src/components/Layout/Page404/index.js
+++ src/components/Layout/Page404/index.js
@@ -1,10 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import { Container } from 'components/UI';
+import Miranha from 'images/gifs/miranha.webp';
+import * as S from './styles';
+
 const Page404 = () => {
   return (
-    <>
-      <main>
-        <h1>404</h1>
-      </main>
-    </>
+    <S.WrapperPage404>
+      <Container as="main" alignment="column">
+        <S.Title>Oops!</S.Title>
+        <S.Subtitle>Página não encontrada :(</S.Subtitle>
+        <img src={Miranha} alt="Homem-Aranha batendo no computador sem resultados" />
+        <div>
+          <Link to={'/'} data-anchor="home" title="Voltar para a home">
+            Voltar para a home
+          </Link>
+        </div>
+      </Container>
+    </S.WrapperPage404>
   );
 };
 
